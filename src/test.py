6483e28dbbf13570ce2ee899192255acb11a72d8
yusufholat <yusufholat@gmail.com>
2021-08-07 23:14:46 +0300

    clean code

--- src/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 13 12:52:23 2021
-
-@author: Ziroo
-"""
-
-for img in test_input_path_list:
-    shutil.copy(img,'../data/test_data/')
--- src/train.py
+++ src/train.py
@@ -1,5 +1,3 @@
-from model import FoInternNet
-from preprocess import tensorize_image, tensorize_mask,image_mask_check
 import glob
 import os
 import numpy as np
@@ -8,20 +6,23 @@ import torch.optim as optim
 import matplotlib.pylab as plt
 import torch
 import tqdm
+from model_unet import FoInternNet
+from preprocess import tensorize_image, tensorize_mask,image_mask_check
 import shutil
 import cv2
+import matplotlib.ticker as mticker
 ######### PARAMETERS ##########
 valid_size = 0.3
 test_size  = 0.1
 batch_size = 4
-epochs = 3
-cuda = False
+epochs = 30
+cuda = True
 input_shape = (224, 224)
 n_classes = 2
 ###############################
 
 # PREPARE IMAGE AND MASK LISTS
-SRC_DIR = os.getcwd()#The method tells us the location of the current working directory (CWD).
+SRC_DIR = os.getcwd()
 ROOT_DIR = os.path.join(SRC_DIR, '..')
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 MASK_DIR = os.path.join(DATA_DIR, 'masks')
@@ -33,14 +34,14 @@ IMAGE_DIR = os.path.join(DATA_DIR, 'images')
 # PREPARE IMAGE AND MASK LISTS
 image_path_list = glob.glob(os.path.join(IMAGE_DIR, '*'))
 image_path_list.sort()
-#The names of the files in the IMAGE_DIR path are listed and sorted
+
 mask_path_list = glob.glob(os.path.join(MASK_DIR, '*'))
 mask_path_list.sort()
 
 # DATA CHECK
 image_mask_check(image_path_list, mask_path_list)
 
-print(image_path_list)
+
 # SHUFFLE INDICES
 indices = np.random.permutation(len(image_path_list))
 
@@ -76,6 +77,8 @@ model = FoInternNet(n_classes=2)
 # DEFINE LOSS FUNCTION AND OPTIMIZER
 criterion =  nn.MSELoss()
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
+# optimizer = optim.RMSprop(model.parameters(),lr=0.002, momentum=0.9)
+# optimizer = optim.Adam(model.parameters(), lr=0.002)
 
 # IF CUDA IS USED, IMPORT THE MODEL INTO CUDA
 if cuda:
@@ -85,9 +88,16 @@ if cuda:
 # TRAINING THE NEURAL NETWORK
 traning_loss_list=[]
 validation_loss_list=[]
+
 for epoch in range(epochs):
-    print('\nEpoch:' + str(epoch+1))
+    print('\nEpoch:' + str(epoch+1) + '/' + str(epochs))
     running_loss = 0
+    
+    #we have to shuffle images and masks so that they are not sync
+    paired_images = list(zip(train_input_path_list,train_label_path_list))
+    np.random.shuffle(paired_images)
+    train_input_path_list, train_label_path_list = zip(*paired_images)
+
     for ind in tqdm.tqdm(range(steps_per_epoch)):
         batch_input_path_list = train_input_path_list[batch_size*ind:batch_size*(ind+1)]
         batch_label_path_list = train_label_path_list[batch_size*ind:batch_size*(ind+1)]
@@ -105,20 +115,20 @@ for epoch in range(epochs):
         running_loss += loss.item()
        
         if ind == steps_per_epoch-1:
-            #for_normalize_run=TF.Normalize(running_loss)
+            
             traning_loss_list.append(running_loss)
-            print('training loss on epoch {}: {}'.format(epoch+1, running_loss))
+            print('\ntraining loss on epoch {}: {}'.format(epoch+1, running_loss))
             val_loss = 0
             for (valid_input_path, valid_label_path) in zip(valid_input_path_list, valid_label_path_list):
                 batch_input = tensorize_image([valid_input_path], input_shape, cuda)
                 batch_label = tensorize_mask([valid_label_path], input_shape, n_classes, cuda)
                 outputs = model(batch_input)
                 loss = criterion(outputs, batch_label)
-                val_loss += loss
+                val_loss += loss.item()
+                validation_loss_list.append(val_loss)
                 break
-            #for_normalize=TF.Normalize(val_loss,(-1,1))
-            validation_loss_list.append(val_loss)
-            print('validation loss on epoch {}: {}'.format(epoch+1, val_loss))
+            
+            print('validation loss on epoch {}: {}'.format(epoch+1, val_loss) + '\n')
 
 for img in test_input_path_list:
     shutil.copy(img,'../data/test_data/')
